f38c3cbd2b742bcfac16278b9955f721dc253a42
AdarshNaikDev <60536396+AdarshNaikDev@users.noreply.github.com>
2022-11-27 01:20:37 +0530

    swap two variables

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-// Import stylesheets
-import './style.css';
+let a =100;
+let b =500;
+[a,b]=[b,a]
+console.log("a is ",a)
+console.log("b is ",b)
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+let a =10;
+let b=20;
+
+let temp;
+
+temp =a;
+a=b;
+b=temp
+console.log(a)
+console.log(b)
+
+ let a =10;
+let b=20;
+
+a = a+b; //a=30
+b = a-b;//b=10
+a= a-b; //a=20
+
+console.log(a)
+
+console.log(b)
