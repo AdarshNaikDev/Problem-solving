e8fab40eabf02607a3540eb6a25161a9b692db90
AdarshNaikDev <60536396+AdarshNaikDev@users.noreply.github.com>
2022-11-27 01:41:25 +0530

    Fizz buzz

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
-let a =100;
-let b =500;
-[a,b]=[b,a]
-console.log("a is ",a)
-console.log("b is ",b)
+const input = 15;
+console.log("execution  started")
+for(let i=1; i<=input; i++)
+{
+  if(i % 3==0 && i % 5 ==0)
+  {
+    console.log("Fizz Buzz");
+  }
+  else if(i%3 ==0)
+  {
+    console.log("Fizz")
+  }
+  else if(i%5 ==0)
+  {
+    console.log("Buzz")
+  }
+  else{
+    console.log(i)
+  }  
 
-let a =10;
-let b=20;
-
-let temp;
-
-temp =a;
-a=b;
-b=temp
-console.log(a)
-console.log(b)
-
- let a =10;
-let b=20;
-
-a = a+b; //a=30
-b = a-b;//b=10
-a= a-b; //a=20
-
-console.log(a)
-
-console.log(b)
+}
+  
+  
